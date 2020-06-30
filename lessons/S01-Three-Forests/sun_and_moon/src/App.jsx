136f73cdf0129dcb2b83fd9db84b70304ba97ac8
Tom-Christian Stokka <tomchristianstokka@gmail.com>
2020-06-29 18:34:42 -0700

    I changed readme

---
+++ lessons/S01-Three-Forests/sun_and_moon/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Import my Custom components
+import SunAndMoon from './SunAndMoon.jsx';
+
+// Create a component
+const App = () => {
+    return (
+        <div>
+            App has changed
+            <SunAndMoon />
+            </div>
+    );
+}
+
+export default App;
---
+++ lessons/S01-Three-Forests/sun_and_moon/src/index.js
@@ -0,0 +1,7 @@
+// These are libraries
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx';
+
+// Inject into our web page
+ReactDOM.render( <App />, document.getElementById('root'));
