8872f7509566666954e66612c5dea983e6dc87f3
Tom-Christian Stokka <tomchristianstokka@gmail.com>
2020-07-15 20:39:50 -0700

    adding lesson work

--- SPA-In-Class/src/App/Footer.jsx
+++ SPA-In-Class/src/App/Footer.jsx
@@ -4,6 +4,25 @@ import styled from 'styled-components';
 import { mq } from '../common/mediaQueries.js';
 
 const Footer = () => {
+
+    // Single line comment
+    /*
+        Multi line comment
+        more than one line
+        see
+    */
+
+    const myVariable = 'I went to the park'
+    const anotherOne = 123;
+    console.log(myVariable, anotherOne);
+
+    const timbuktu = () => {
+        // Actions to execute
+        console.log('Executing timbuktu');
+    }
+
+    timbuktu();
+
     return (
         <FooterStyled>
             <div className='nested-wrapper'>
