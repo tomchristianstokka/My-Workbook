6b735648e5fe36148f9c98bca7391ed32cd9a3b8
Tom-Christian Stokka <tomchristianstokka@gmail.com>
2020-08-05 20:43:03 -0700

    Monday lesson

--- SPA-In-Class/src/App/Pages/Services/Gallery/GalleryItem.jsx
+++ SPA-In-Class/src/App/Pages/Services/Gallery/GalleryItem.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import Lightbox from './Lightbox.jsx';
+import Lightbox from '../../../../common/Lightbox.jsx';
 
 const GalleryItem = ({ item }) => {
 
@@ -24,7 +24,7 @@ const GalleryItem = ({ item }) => {
                     onClick={ turnOn }
                 />
                 <div className='title'>{item.title}</div>
-                <Lightbox show={ showLBox } hideAction={ turnOff }> 
+                <Lightbox show={ showLBox } hideAction={ turnOff } width={ 300 }> 
                     <img src={ item.image } alt={ item.title } />
                     <h3>{ item.title }</h3>
                     <div className='description'>
@@ -44,7 +44,11 @@ const GalleryItemStyled = styled.div`
     border: solid 1px purple;
 
     margin: 20px;
-    img { display: block; }
+    img { 
+        display: block; 
+        border: solid 10px purple;
+        
+    }
 
     .category {
         position: absolute;
@@ -63,4 +67,19 @@ const GalleryItemStyled = styled.div`
         padding: 10px;
         font-size: 12px;
     } 
+
+    .Lightbox {
+        img {
+            border: solid 20px red;
+            max-width: 100%;
+        }
+        h3 {
+            font-size: 30px;
+            margin: 0px;
+        }
+        .description {
+            color: #333;
+            background-color: gray;
+        }
+    }
 `;
